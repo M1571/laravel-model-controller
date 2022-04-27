f97fe984e913f4ae5b81587c472cd24f25a5dd83
M1571 <94639639+M1571@users.noreply.github.com>
2022-04-27 11:40:07 +0200

    end

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@
     /*! no static exports found */
     /***/ (function(module, exports, __webpack_require__) {
     
-    __webpack_require__(/*! C:\Users\michelangelo\Desktop\BOOLEAN\laravel-comics\resources\js\app.js */"./resources/js/app.js");
-    module.exports = __webpack_require__(/*! C:\Users\michelangelo\Desktop\BOOLEAN\laravel-comics\resources\sass\app.scss */"./resources/sass/app.scss");
+    __webpack_require__(/*! C:\Users\michelangelo\Desktop\BOOLEAN\laravel-model-controller\resources\js\app.js */"./resources/js/app.js");
+    module.exports = __webpack_require__(/*! C:\Users\michelangelo\Desktop\BOOLEAN\laravel-model-controller\resources\sass\app.scss */"./resources/sass/app.scss");
     
     
     /***/ })
